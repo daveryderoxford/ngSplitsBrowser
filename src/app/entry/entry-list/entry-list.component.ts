15b51250c244f8349cbc9688b9fe4090dbf7a36e
daveryderoxford <dave@theryderclan.co.uk>
2020-04-04 22:07:57 +0100

    Fixed entry compoent strict compilation

--- src/app/entry/entry-list/entry-list.component.ts
+++ src/app/entry/entry-list/entry-list.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { EntryService } from 'app/entry/entry.service';
-import { Entry, FixtureDetailsAndEntries, EntryCourse } from 'app/model/entry';
-import { Course } from 'app/results/model';
-import { map, switchMap } from 'rxjs/operators';
 import { UntilDestroy } from '@ngneat/until-destroy';
+import { EntryService } from 'app/entry/entry.service';
+import { Entry, EntryCourse, FixtureDetailsAndEntries } from 'app/model/entry';
+import { map, switchMap } from 'rxjs/operators';
 
 /** Display all the entries for a fixture */
 @UntilDestroy( { checkProperties: true } )
-@Component({
+@Component( {
    selector: 'app-entry-list',
    templateUrl: './entry-list.component.html',
    styleUrls: ['./entry-list.component.scss'],
-})
+} )
 export class EntryListComponent implements OnInit {
 
    fixture: FixtureDetailsAndEntries;
@@ -20,32 +19,36 @@ export class EntryListComponent implements OnInit {
 
    displayedColumns = ["id", "name", "club", "class", ];
 
-   constructor(private route: ActivatedRoute,
-      private es: EntryService) { }
+   constructor ( private route: ActivatedRoute,
+      private es: EntryService ) { }
 
    ngOnInit() {
 
-         this.route.paramMap.pipe(
-         map(params => params.get('id')),
-         switchMap(fixtureId => this.es.getEntries$(fixtureId))
-     ).subscribe( entry => {
-           this.fixture = entry;
-           this.applyFilter("");
-           this.entries = this.fixture.entries;
-      });
+      this.route.paramMap.pipe(
+         map( params => params.get( 'id' ) ),
+         switchMap( fixtureId => this.es.getEntries$( fixtureId ) )
+      ).subscribe( entry => {
+         this.fixture = entry;
+         this.applyFilter( "" );
+         this.entries = this.fixture.entries;
+      } );
    }
 
-   applyFilter(event: any) {
-     const filterValue = event.target.value;
-     const str = filterValue.trim().toLowerCase();
-      this.entries = this.fixture.entries.filter( (entry) => {
-         return entry.firstname.toLowerCase().startsWith( str) ||
-            entry.surname.toLowerCase().startsWith(str) ||
-            entry.club.toLowerCase().startsWith(str);
-     });
+   setFilter( event: KeyboardEvent) {
+      const target = event.target as HTMLTextAreaElement;
+      this.applyFilter( target.value);
    }
 
-   entriesForCourse(course: EntryCourse): Entry[] {
-      return this.entries.filter((entry) => entry.course === course.name);
+   applyFilter( filterValue: string ) {
+      const str = filterValue.trim().toLowerCase();
+      this.entries = this.fixture.entries.filter( ( entry ) => {
+         return entry.firstname.toLowerCase().startsWith( str ) ||
+            entry.surname.toLowerCase().startsWith( str ) ||
+            entry.club.toLowerCase().startsWith( str );
+      } );
+   }
+
+   entriesForCourse( course: EntryCourse ): Entry[] {
+      return this.entries.filter( ( entry ) => entry.course === course.name );
    }
 }
