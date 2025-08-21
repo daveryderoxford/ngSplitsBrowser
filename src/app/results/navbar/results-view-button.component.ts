4ae3efdfe7cd164cecf46ff94c542b8a9d7a7060
daveryderoxford <dave@theryderclan.co.uk>
2025-08-21 11:58:40 +0100

    View selection more mobile friendly

--- src/app/results/navbar/results-view-button.component.ts
+++ src/app/results/navbar/results-view-button.component.ts
@@ -1,15 +1,30 @@
 /** Pure component to show button groupto allow results view to be selected */
 
-import { Component, input, output } from '@angular/core';
-import { ResultsView } from '../model';
+import { Component, inject, input, output } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { AppBreakpoints } from 'app/shared/services/breakpoints';
+import { ResultsView } from '../model';
 
 @Component({
     selector: 'app-results-view-button',
     template: `
+    @if (hs.narrowScreen()) {
+      <mat-form-field appearance="outline" subscriptSizing="dynamic" class="dense-form-field set-width">
+        <mat-label>View</mat-label>
+        <mat-select [value]="selectedView()" (selectionChange)="buttonClicked($event.value)">
+          <mat-select-trigger>
+            {{ selectedView().name }}
+          </mat-select-trigger>
+          @for (view of views(); track view.name) {
+            <mat-option [value]="view">{{ view.name }}</mat-option>
+          }
+        </mat-select>
+      </mat-form-field> 
+    } @else {
       <mat-button-toggle-group [hideSingleSelectionIndicator]=true>
         @for (view of views(); track view.name) {
           <mat-button-toggle [checked]="selectedView().type === view.type" (click)="buttonClicked(view)">
@@ -17,17 +32,24 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
           </mat-button-toggle>
         }
       </mat-button-toggle-group>
+    }
   `,
-    styles: ``,
-    imports: [MatButtonToggleModule, MatMenuModule, MatIconModule]
+    styles: `
+    .set-width {
+      min-width: 100px;
+      max-width: 100px;
+    }`,
+  imports: [MatButtonToggleModule, MatMenuModule, MatIconModule, MatSelectModule, MatFormFieldModule]
 })
 export class ResultsViewButtonComponent {
+  protected hs = inject(AppBreakpoints)
 
   views = input<ResultsView[]>([]);
   selectedView = input<ResultsView>();
   viewSelected = output<ResultsView>();
 
   buttonClicked(view: ResultsView) {
+    console.log(`View selected: ${view.name}`);
     this.viewSelected.emit(view);
   }
 }
